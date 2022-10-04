d36116edb077fe74828ef06045bcfeb59ed0a803
Lila Sarfson <lilamandarina@hotmail..com>
2022-10-04 17:06:18 +0200

    localstorage funcional

--- src/components/CinemaSit.vue
+++ src/components/CinemaSit.vue
@@ -12,48 +12,43 @@
         arrayOfPlaces:this.seatsData
       }
     },
-    mounted(){
-      if(localStorage.seatsSelected)
-      this.seatsSelected=localStorage.seatsSelected
+    created(){ 
+      if(localStorage.getItem('sitios')!='')
+      this.seatsSelected=JSON.parse(localStorage.getItem('sitios'))
     },
     watch:{
       currentMoviePrice:function() {
       this.movieChanged()},
-
-      seatsSelected:function(){
-        localStorage.seatsSelected=this.seatsSelected
-      }
    },
     methods:{
       movieChanged(){
         this.totalPrice=this.currentMoviePrice*this.numberSitSelected
       },
-     takeSit(id){
-    this.arrayOfPlaces[id].selected= !this.arrayOfPlaces[id].selected;
-    if(!this.seatsSelected.includes(this.arrayOfPlaces[id])) 
-    // Poner condiciones para el push y filter
-    {this.seatsSelected.push(this.arrayOfPlaces[id])
-      if (this.arrayOfPlaces[id].selected === false){
-      this.seatsSelected.filter((seat)=> seat.selected===true)
+     takeSeat(id){
+    this.arrayOfPlaces[id].selected= !this.arrayOfPlaces[id].selected;    
+    if(this.arrayOfPlaces[id].selected===false && this.arrayOfPlaces[id].occupied===false ){
+      this.numberSitSelected= this.numberSitSelected-1;
+      this.totalPrice= this.totalPrice-this.currentMoviePrice
+      if(this.seatsSelected.includes(this.arrayOfPlaces[id]))
+      {this.seatsSelected = this.seatsSelected.filter((seat)=> seat !== this.arrayOfPlaces[id])}
     }
+    else if (this.arrayOfPlaces[id].selected===true && this.arrayOfPlaces[id].occupied===false){
+      this.numberSitSelected= this.numberSitSelected+1;
+      this.totalPrice=this.currentMoviePrice*this.numberSitSelected;
+      if(!this.seatsSelected.includes(this.arrayOfPlaces[id]))
+      {this.seatsSelected.push(this.arrayOfPlaces[id])}
     }
-    console.log(this.seatsSelected)
-      if(this.arrayOfPlaces[id].selected===false && this.arrayOfPlaces[id].occupied===false ){
-        this.numberSitSelected= this.numberSitSelected-1;
-        this.totalPrice= this.totalPrice-this.currentMoviePrice
-      }
-      else if (this.arrayOfPlaces[id].selected===true && this.arrayOfPlaces[id].occupied===false){
-        this.numberSitSelected= this.numberSitSelected+1;
-        this.totalPrice=this.currentMoviePrice*this.numberSitSelected
-      }
-      console.log(this.totalPrice)
-     }
-  }
+    this.addItemLstorage();
+  },
+  addItemLstorage(){
+    localStorage.setItem('sitios', JSON.stringify(this.seatsSelected))
+  },
+}
 }
 </script>
     <template>
     <div class="grid grid-cols-4 gap-2 items-start">
-      <div v-for="place in arrayOfPlaces" :key="place.id" @click="takeSit(place.id)" :class="(place.occupied) ? 'bg-white-cinema': (place.selected) ? 'bg-blueSpace':'bg-seat'" class="h-smallseat w-4 rounded-t-xl">
+      <div v-for="place in arrayOfPlaces" :key="place.id" @click="takeSeat(place.id)" :class="(place.occupied) ? 'bg-white-cinema': (place.selected) ? 'bg-blueSpace':'bg-seat'" class="h-smallseat w-4 rounded-t-xl">
       </div>
       
     </div>
